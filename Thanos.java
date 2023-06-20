d84254689e49905cc9824a93ec2ecbc81f5b5e0b
Daniel Thibaut <dthibaut1@free.fr>
2023-06-21 00:22:48 +0200

    Suppress comments in Thanos.java

--- Thanos.java
+++ Thanos.java
@@ -15,14 +15,6 @@ public class Thanos {
         heroes.add(new Hero("Thor", 1500));
         heroes.add(new Hero("Hulk", 49));
         heroes.add(new Hero("Doctor Strange", 42));
-//        heroes.add(blackWidow);
-//        heroes.add(captainAmerica);
-//        heroes.add(vision);
-//        heroes.add(ironMan);
-//        heroes.add(scarlettWitch);
-//        heroes.add(thor);
-//        heroes.add(hulk);
-//        heroes.add(doctorStrange);
         // TODO 3 : It's Thor birthday, now he's 1501
         heroes.get(5).setAge(1501);
         // TODO 4 : Shuffle the heroes list
